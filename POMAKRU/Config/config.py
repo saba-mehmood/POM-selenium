0149dc271fd2afd7ae3785d5f50e4c27278f5544
saba-mehmood <68803765+saba-mehmood@users.noreply.github.com>
2022-02-03 23:23:58 +0500

    Update config.py

--- POMAKRU/Config/config.py
+++ POMAKRU/Config/config.py
@@ -4,3 +4,13 @@ class TestData:
     YOPMAIL_URL="https://yopmail.com/en/wm"
     EMAIL="xyz@yopmail.com"
     LOGIN_PAGE_TITLE=""
+    
+    EMAIL_SIGNUP='avax1123@yopmail.com'
+    FIRST_NAME='Sara'
+    LAST_NAME='Khan'
+    ADDRESS='Ohio US'
+    CITY='Ohio'
+    ZIP_CODE='45698'
+    PHONE_NO='2365247895'
+    OTP='https://avaxdevapi.akru.co/api/user/showOtp/'+EMAIL_SIGNUP
+    SSN = '236547852'
